f3ada41959383b47104abbfd42615a794cf963b3
ngan <ngan@azebiz.com>
2020-03-21 22:04:02 +0700

    landing-page: course

--- resources/views/home-page/home.blade.php
+++ resources/views/home-page/home.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.master')
 @section('content')
-  <div class="row">
-    <div class="col-sm-12 col-md-8 col-lg-8 column-main">
-      @include('home-page.partials.events')
-{{--      @include('home-page.partials.news')--}}
-{{--      @include('home-page.partials.books')--}}
+    <div class="row">
+        <div class="col-sm-12 col-md-8 col-lg-8 column-main">
+            @include('home-page.partials.events')
+            @include('home-page.partials.news')
+            @include('home-page.partials.books')
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 sidebar">
+            @include('home-page.partials.sidebar-info')
+            @include('home-page.partials.chart')
+            @include('home-page.partials.chart-major')
+        </div>
     </div>
-    <div class="col-sm-12 col-md-4 col-lg-4 sidebar">
-      @include('home-page.partials.sidebar-info')
-      @include('home-page.partials.chart')
-      @include('home-page.partials.chart-major')
-    </div>
-  </div>
 @endsection
