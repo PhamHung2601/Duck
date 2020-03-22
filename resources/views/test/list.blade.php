b1b542ee6c2aaa5e20b890c860a0e644e2493888
congam <anmanhcong@gmail.com>
2020-03-22 09:45:15 +0700

    sua giao dien test

--- resources/views/test/list.blade.php
+++ resources/views/test/list.blade.php
@@ -5,12 +5,12 @@
             <div style="background-color:#f2f2f2;padding:10px"><b><i class="fa fa-bars"></i> Danh sách đề thi
                     năm {{$year}}</b></div>
             @foreach ($tests as $test)
-                <p style="padding:8px">
+                <p style="padding:8px;margin-bottom: 0px">
                     <span style="color:#666666;padding-right:5px"> 1. </span>
                     <a href="/test/view/{{$test->id}}"
                        title="{{$test->title}}" style="margin-right:15px">
                         <strong>{{$test->title}}</strong></a>
-                    <span style="color:#666666; font-size:12px">{{$test->created_at}}</span>
+                    <span style="color:#666666; font-size:12px"><img src="{{url('/img/iconword.png')}}" height="18px" style="margin-right: 5px">{{$test->created_at}}</span>
                 </p>
             @endforeach
         </section>
