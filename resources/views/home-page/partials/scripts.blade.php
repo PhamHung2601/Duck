993a562b8016adef159dfabb070211ad28bf8c96
Loky <doankhachung.bk@gmail.com>
2020-03-26 01:34:36 +0700

    Product View Page : facebook plugin

--- resources/views/home-page/partials/scripts.blade.php
+++ resources/views/home-page/partials/scripts.blade.php
@@ -1,3 +1,4 @@
 <script src="{{ asset('vendor/landing-page/js/jquery.min.js') }}"></script>
 
 <script src="{{ asset('vendor/landing-page/js/bootstrap.bundle.min.js') }}"></script>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0&appId=424107491618576&autoLogAppEvents=1"></script>
