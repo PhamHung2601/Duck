b3c24109ae1a1cc6e3bd674bbe807bca8a73fc5b
Trang <trang@azebiz.com>
2020-03-28 18:56:25 +0700

    product - list

---
+++ resources/views/book/partials/list.blade.php
@@ -0,0 +1,24 @@
+<div class="product-item">
+    <div class="product-item-inner">
+        <div class="product-img">
+            <a href="#" class="text-center">
+                <img
+                     src="//product.hstatic.net/1000245066/product/artboard_8_e91f7f31eb3e4933aa4c7eda4006a49f_compact.jpg"
+                     alt="Tips and Tricks - Mẹo nhỏ có võ dành cho Marketer">
+            </a>
+        </div>
+        <div class="product-info">
+            <div class="product-title">
+                <a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</a>
+            </div>
+            <div class="price-box clearfix">
+                <span class="price-wrapper">
+                       <span class="special-price">
+                    <span class="price">109,000₫</span>
+                </span>
+                <span class="old-price"><span class="price">129,000₫</span></span>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
