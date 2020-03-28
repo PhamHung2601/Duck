fb5f9de1b6a0f74c6a092dc1bddda201ef54da17
ngan <ngan@azebiz.com>
2020-03-28 23:41:47 +0700

    introduction landing-page

---
+++ resources/views/landing-page/introduction/partials/hiring_section.blade.php
@@ -0,0 +1,38 @@
+<section class="landing-page-section hiring-section">
+    <h2 class="section-title">Tuyen Dung</h2>
+    <div class="section-content display-flex-content">
+        <div class="col-4 hiring-content">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 hiring-content">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 hiring-content">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button class="hiring-more">
+        <a href="{{url("home")}}/about/hiring">Xem Them</a>
+    </button>
+</section>
