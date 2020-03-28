2a454bdfb37834c853beeb6bbf3fc745f6b8aa9b
congam <anmanhcong@gmail.com>
2020-03-29 00:26:58 +0700

    test list

--- resources/views/news/view.blade.php
+++ resources/views/news/view.blade.php
@@ -3,23 +3,27 @@
     @include('static-block.content-top')
 @endsection
 @section('content')
-<div id="news-{{$news->id}}" class="et_pb_post clearfix news-{{$news->id}}">
-    <div class="image-container">
-        <h1 class="entry-title text-center">{{$news->title}}</h1>
-        <div class="text-left">
-            <span>Thể thao</span>
-        </div>
-        <div class="text-center">
-            <a href="https://stepup.edu.vn/blog/cau-truc-spend/" class="entry-featured-image-url">
-                <img src="https://cdnstepup.r.worldssl.net/wp-content/uploads/2020/03/Cấu-trúc-spend-trong-tiếng-Anh-400x250.png" width="70%" alt="{{$news->title}}">
-            </a>
+    <div id="news-{{$news->id}}" class="et_pb_post clearfix news-{{$news->id}}">
+        <div class="image-container-blog pd-10">
+            <h1 class="entry-title text-center">{{$news->title}}</h1>
+            <div class="text-left pd-10">
+                <div>
+                    <a href="#" class="tag-item">Thể thao</a>
+                    <a href="#" class="tag-item">Miền trung</a>
+                    <a href="#" class="tag-item">Nam cực</a>
+                </div>
+                <div>
+                    <a href="https://stepup.edu.vn/blog/cau-truc-spend/" class="entry-featured-image-url">
+                        <img src="https://cdnstepup.r.worldssl.net/wp-content/uploads/2020/03/Cấu-trúc-spend-trong-tiếng-Anh-400x250.png" width="100%" alt="{{$news->title}}">
+                    </a>
+                </div>
+                <p class="post-meta mt-1"><span class="published-time">Mar 18, 2020</span></p>
+            </div>
+            <div>
+                <p>{{$news->description}}</p>
+            </div>
         </div>
     </div>
-    <p class="post-meta">  <span class="published">Mar 18, 2020</span></p>
-    <div>
-        <p>{{$news->description}}</p>
-    </div>
-</div>
 @endsection
 @section('content-bottom')
     @include('static-block.content-bottom')
