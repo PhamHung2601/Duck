18b8f18f01a47bc36876ec4564734e182a2e6f14
Sam <sam@tigren.com>
2020-03-28 09:52:36 +0700

    fix js.

--- resources/views/home-page/partials/scripts.blade.php
+++ resources/views/home-page/partials/scripts.blade.php
@@ -1,6 +1,7 @@
 
 
 <script src="{{ asset('vendor/landing-page/js/bootstrap.bundle.min.js') }}"></script>
+<script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.slick/1.3.15/slick.min.js"></script>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0&appId=424107491618576&autoLogAppEvents=1"></script>
 <script src="http://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
