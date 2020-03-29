8f007d8a57d2672a01918b563fff72df4caf6034
ngan <ngan@azebiz.com>
2020-03-29 11:12:10 +0700

    introduction landing-page

--- resources/views/landing-page/introduction/partials/who_I_am.blade.php
+++ resources/views/landing-page/introduction/partials/who_I_am.blade.php
@@ -1,7 +1,7 @@
-<section class="landing-page-section who-I-am-section">
+<section class="landing-page-section who-I-am-section" style="background-image: url('{{ asset('img/about_me_bg.png')}}')">
+    <h1 class="section-title">"Sieu Thu Linh" Dam Thanh Tung?</h1>
     <div class="section-content display-flex-content">
         <div class="col-4 me-avatar">
-            <h3>Sieu Thu Linh Dam Thanh Tung?</h3>
             <img src="{{asset("img/anhTung.jpg")}}">
         </div>
         <div class="me-content col-8">
@@ -54,27 +54,27 @@
     <div class="section-content">
         <h3 class="section-title">Bao Chi Viet Gi Ve Thay Tung?</h3>
         <div class="display-flex-content">
-            <div class="col-5">
+            <div class="col-3">
                 <div class="news">
                     <h4>Chàng “Siêu thủ lĩnh” 24 tuổi với dự án “Xoá mù Địa lý”</h4>
                     <p>Đàm Thanh Tùng - một cái tên không còn quá xa lạ với các bạn trẻ Hà Nội. Thanh Tùng nổi tiếng khi
                         tham gia chương trình “Siêu thủ lĩnh” do VTV6 tổ chức với đề tài: “Biến các bãi rác thành vườn
                         hoa” ...
                     </p>
-                    <a href="http://tv.nguoiduatin.vn/2/chang-sieu-thu-linh-voi-du-an-xoa-mu-dia-ly-1238.html">Xem
-                        Them</a>
+                    <button><a href="http://tv.nguoiduatin.vn/2/chang-sieu-thu-linh-voi-du-an-xoa-mu-dia-ly-1238.html">Xem
+                        Them</a></button>
                 </div>
-                <hr>
                 <div class="news">
                     <h4>Thầy giáo Địa lý chia sẻ bí quyết ôn tập 'nước rút' môn kỳ thi THPT Quốc gia 2019</h4>
                     <p>Nhận xét về đề thi tham khảo môn Địa lý năm nay, thầy giáo Đàm Thanh Tùng, tác giả cuốn sách “Xử
                         lý nhanh 30 đề THPTQG môn Địa lý” cho biết: “Đề thi năm nay vẫn như những năm trước, với 40 câu
                         trắc nghiệm. Cấu trúc đề thi cơ bản vẫn thuộc hai phần kiến thức lớp 11 và 12 ...
                     </p>
-                    <a href="https://baomoi.com/thay-giao-dia-ly-chia-se-bi-quyet-on-tap-nuoc-rut-mon-ky-thi-thpt-quoc-gia-2019/c/30844696.epi">Xem
-                        Them</a>
+                    <button><a href="https://baomoi.com/thay-giao-dia-ly-chia-se-bi-quyet-on-tap-nuoc-rut-mon-ky-thi-thpt-quoc-gia-2019/c/30844696.epi">Xem
+                        Them</a></button>
                 </div>
-                <hr>
+            </div>
+            <div class="col-3">
                 <div class="news">
                     <h4>Sau thành công của dự án vì cộng đồng “biến bãi rác thành vườn hoa”, thủ lĩnh nhóm quyết định
                         khởi nghiệp
@@ -83,10 +83,9 @@
                         chúng tôi dường như vẫn là chàng trai 23 tuổi trường Báo đầy nhiệt huyết từng tham gia “Siêu thủ
                         lĩnh” năm nào ...
                     </p>
-                    <a href="https://we25.vn/cong-so/sau-thanh-cong-cua-du-an-vi-cong-dong-bien-bai-rac-thanh-vuon-hoa-thu-linh-nhom-quyet-dinh-khoi-nghiep-209924">Xem
-                        Them</a>
+                    <button><a href="https://we25.vn/cong-so/sau-thanh-cong-cua-du-an-vi-cong-dong-bien-bai-rac-thanh-vuon-hoa-thu-linh-nhom-quyet-dinh-khoi-nghiep-209924">Xem
+                        Them</a></button>
                 </div>
-                <hr>
                 <div class="news">
                     <h4>Chàng trai đam mê biến bãi rác thành vườn hoa
                     </h4>
@@ -95,12 +94,11 @@
                         sẻ về dự án cải tạo những bãi rác tự phát lâu năm thành những vườn hoa thơm mát cùng những người
                         cộng tác viên nhiệt tình trong nhóm...
                     </p>
-                    <a href="http://anninhthudo.vn/doi-song/chang-trai-dam-me-bien-bai-rac-thanh-vuon-hoa/706596.antd">Xem
-                        Them</a>
+                    <button><a href="http://anninhthudo.vn/doi-song/chang-trai-dam-me-bien-bai-rac-thanh-vuon-hoa/706596.antd">Xem
+                        Them</a></button>
                 </div>
             </div>
-            <div class="col-2"></div>
-            <div class="col-5">
+            <div class="col-3">
                 <div class="news">
                     <h4>
                         Hành trình từ “siêu thủ lĩnh” đến vai trò người truyền cảm hứng của chàng trai đam mê … với
@@ -109,32 +107,9 @@
                         tới khi đảm nhận vai trò người truyền cảm hứng cho nhiều người trẻ khác chung tay bảo vệ môi
                         trường đều bắt nguồn từ … rác thải ...
                     </p>
-                    <a href="https://baotainguyenmoitruong.vn/moi-truong/hanh-trinh-tu-sieu-thu-linh-den-vai-tro-nguoi-truyen-cam-hung-cua-chang-trai-dam-me-voi-rac-1253032.html">Xem
-                        Them</a>
+                    <button><a href="https://baotainguyenmoitruong.vn/moi-truong/hanh-trinh-tu-sieu-thu-linh-den-vai-tro-nguoi-truyen-cam-hung-cua-chang-trai-dam-me-voi-rac-1253032.html">Xem
+                        Them</a></button>
                 </div>
-                <hr>
-                <div class="news">
-                    <h4>Gặp gỡ chàng sinh viên Báo chí – Một trong số tác giả của 4 vườn hoa "mọc lên" từ bãi rác ở Hà
-                        Nội</h4>
-                    <p>Có những bãi rác sinh hoạt đã tồn tại 5, 10 năm, thậm chí nhiều hơn thế, chính quyền và người dân
-                        đã dùng mọi cách nhưng vẫn không thể dọn sạch triệt để. Thế nhưng, nhóm bạn trẻ đã đồng lòng “hô
-                        biến” chúng thành những vườn hoa xinh đẹp ...
-                    </p>
-                    <a href="http://kenh14.vn/gap-go-chang-sinh-vien-bao-chi-mot-trong-so-tac-gia-cua-4-vuon-hoa-moc-len-tu-bai-rac-o-ha-noi-20160907214821754.chn">Xem
-                        Them</a>
-                </div>
-                <hr>
-                <div class="news">
-                    <h4>Tham gia Siêu thủ lĩnh là trở về với “mối tình xưa”</h4>
-                    <p>Đó là chia sẻ của chàng sinh viên Đàm Thanh Tùng - một trong những thí sinh đặc biệt
-                        của chương trình Siêu thủ lĩnh 2016 về hành trình vừa trải qua. Với sức trẻ, sự
-                        nhiệt tình và đam mê tình nguyện, Tùng và nhóm bạn của mình đã biến bãi rác đối
-                        diện Nhà tang lễ Bệnh viện Công an 19/8, Hà Nội thành một vườn hoa tươi đẹp ...
-                    </p>
-                    <a href="http://tv.nguoiduatin.vn/2/chang-sieu-thu-linh-voi-du-an-xoa-mu-dia-ly-1238.html">Xem
-                        Them</a>
-                </div>
-                <hr>
                 <div class="news">
                     <h4>Đàm Thanh Tùng - Thủ lĩnh biến bãi rác thành vườn hoa
                     </h4>
@@ -142,8 +117,29 @@
                         viên đang theo học kiến trúc, báo chí, văn hoá, môi trường... Mặc dù, học các chuyên ngành khác
                         nhau, họ đều hào hứng làm việc cùng nhau dưới sự dẫn dắt của thủ lĩnh trẻ Thanh Tùng....
                     </p>
-                    <a href="http://hanoitv.vn/dam-thanh-tung---thu-linh-bien-rac-thanh-vuon-hoa-d51849.html">Xem
-                        Them</a>
+                    <button><a href="http://hanoitv.vn/dam-thanh-tung---thu-linh-bien-rac-thanh-vuon-hoa-d51849.html">Xem
+                            Them</a></button>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="news">
+                    <h4>Gặp gỡ chàng sinh viên Báo chí – Một trong số tác giả của 4 vườn hoa "mọc lên" từ bãi rác ở Hà
+                        Nội</h4>
+                    <p>Có những bãi rác sinh hoạt đã tồn tại 5, 10 năm, thậm chí nhiều hơn thế, chính quyền và người dân
+                        đã dùng mọi cách nhưng vẫn không thể dọn sạch triệt để. Thế nhưng, nhóm bạn trẻ đã đồng lòng “hô
+                        biến” chúng thành những vườn hoa xinh đẹp ...
+                    </p>
+                    <button><a href="http://kenh14.vn/gap-go-chang-sinh-vien-bao-chi-mot-trong-so-tac-gia-cua-4-vuon-hoa-moc-len-tu-bai-rac-o-ha-noi-20160907214821754.chn">Xem Them</a></button>
+                </div>
+                <div class="news">
+                    <h4>Tham gia Siêu thủ lĩnh là trở về với “mối tình xưa”</h4>
+                    <p>Đó là chia sẻ của chàng sinh viên Đàm Thanh Tùng - một trong những thí sinh đặc biệt
+                        của chương trình Siêu thủ lĩnh 2016 về hành trình vừa trải qua. Với sức trẻ, sự
+                        nhiệt tình và đam mê tình nguyện, Tùng và nhóm bạn của mình đã biến bãi rác đối
+                        diện Nhà tang lễ Bệnh viện Công an 19/8, Hà Nội thành một vườn hoa tươi đẹp ...
+                    </p>
+                    <button><a href="http://tv.nguoiduatin.vn/2/chang-sieu-thu-linh-voi-du-an-xoa-mu-dia-ly-1238.html">Xem
+                        Them</a></button>
                 </div>
             </div>
         </div>
