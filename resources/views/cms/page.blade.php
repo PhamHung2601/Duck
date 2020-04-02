53379279f982cddc3ee22720af2ea7bcb90aaf19
Sam <sam@tigren.com>
2020-04-02 17:19:43 +0700

    cms page

--- resources/views/cms/page.blade.php
+++ resources/views/cms/page.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.master')
 @section('content')
+    <div class="title">
+        <h3>{{ $cmsPageModel->title }}</h3>
+    </div>
     <div class="content">
         {!! $cmsPageContent !!}
     </div>
