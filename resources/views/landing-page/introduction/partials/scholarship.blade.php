2fa6cc42355d9b3845ae287136ab2abb33a7faea
Trang <trang@azebiz.com>
2020-04-04 19:15:44 +0700

    landing about/scholarship

--- resources/views/landing-page/introduction/partials/scholarship.blade.php
+++ resources/views/landing-page/introduction/partials/scholarship.blade.php
@@ -1,13 +1,20 @@
 <section class="landing-page-section hiring">
     {!! \Helper::getStaticBlockContentById("scholarship-page") !!}
-{{--    <h1 class="section-title">HỌC BỔNG AT FOUNDATION</h1>--}}
-{{--    <div class="section-content">--}}
-{{--        <p>Step Up tin rằng mỗi người đều có những tiềm năng vô hạn để trở nên giỏi giang. Vấn đề là họ không áp dụng--}}
-{{--            đúng phương pháp để việc học hiệu quả hơn. Vì vậy Step Up mong muốn giúp cho những người gặp khó khăn trong--}}
-{{--            việc học hành nói chung và học Tiếng Anh nói riêng được tiếp cận các phương pháp, kinh nghiệm học tiếng Anh--}}
-{{--            thông minh để trở nên giỏi thực sự.</p>--}}
-{{--    </div>--}}
-{{--    <button>--}}
-{{--        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfkhiyp929MFEsWZ2MjvU2bJX0MHRRMZnpG2d481p3swdNK_w/viewform">Tim Hieu Them</a>--}}
-{{--    </button>--}}
+    {{--<div class="hirring-banner">--}}
+        {{--<div class="landing-page-section-wrapper">--}}
+            {{--<h1 class="section-title">HỌC BỔNG AT FOUNDATION</h1>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+
+    {{--<div class="landing-page-section-wrapper hirring-content">--}}
+        {{--<div class="section-content">--}}
+            {{--<p>Step Up tin rằng mỗi người đều có những tiềm năng vô hạn để trở nên giỏi giang. Vấn đề là họ không áp dụng--}}
+                {{--đúng phương pháp để việc học hiệu quả hơn. Vì vậy Step Up mong muốn giúp cho những người gặp khó khăn trong--}}
+                {{--việc học hành nói chung và học Tiếng Anh nói riêng được tiếp cận các phương pháp, kinh nghiệm học tiếng Anh--}}
+                {{--thông minh để trở nên giỏi thực sự.</p>--}}
+        {{--</div>--}}
+        {{--<div class="box-action">--}}
+            {{--<a class="landing-button" href="https://docs.google.com/forms/d/e/1FAIpQLSfkhiyp929MFEsWZ2MjvU2bJX0MHRRMZnpG2d481p3swdNK_w/viewform">Tim Hieu Them</a>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 </section>
