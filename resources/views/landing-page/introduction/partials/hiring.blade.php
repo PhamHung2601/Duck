3442a4c39ec7c82695d21f5f55f50fcc58964235
Trang <trang@azebiz.com>
2020-04-04 18:59:26 +0700

    landing  about/hiring

--- resources/views/landing-page/introduction/partials/hiring.blade.php
+++ resources/views/landing-page/introduction/partials/hiring.blade.php
@@ -1,11 +1,16 @@
 <section class="landing-page-section hiring">
-    <div class="landing-page-section-wrapper">
-        <h1 class="section-title" style="margin-bottom: 30px">Tuyen Dung</h1>
-        <div class="section-content">
-            <p>Step Up tin rằng mỗi người đều có những tiềm năng vô hạn để trở nên giỏi giang. Vấn đề là họ không áp dụng
-                đúng phương pháp để việc học hiệu quả hơn. Vì vậy Step Up mong muốn giúp cho những người gặp khó khăn trong
-                việc học hành nói chung và học Tiếng Anh nói riêng được tiếp cận các phương pháp, kinh nghiệm học tiếng Anh
-                thông minh để trở nên giỏi thực sự.</p>
+    <div class="hirring-banner">
+        <div class="landing-page-section-wrapper">
+        <h1 class="section-title">Tuyen Dung</h1>
         </div>
     </div>
+
+    <div class="landing-page-section-wrapper hirring-content">
+            <div class="section-content">
+                <p>Step Up tin rằng mỗi người đều có những tiềm năng vô hạn để trở nên giỏi giang. Vấn đề là họ không áp dụng
+                    đúng phương pháp để việc học hiệu quả hơn. Vì vậy Step Up mong muốn giúp cho những người gặp khó khăn trong
+                    việc học hành nói chung và học Tiếng Anh nói riêng được tiếp cận các phương pháp, kinh nghiệm học tiếng Anh
+                    thông minh để trở nên giỏi thực sự.</p>
+            </div>
+        </div>
 </section>
