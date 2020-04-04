5f69b092a4ee84734ff0a4d50bc5c3b3385f5f2f
Trang <trang@azebiz.com>
2020-04-04 20:02:01 +0700

    landing about/students - re-commit

--- resources/views/landing-page/introduction/partials/feeling.blade.php
+++ resources/views/landing-page/introduction/partials/feeling.blade.php
@@ -1,145 +1,38 @@
 <section class="landing-page-section feeling" id="feeling_section">
     {!!\Helper::getStaticBlockContentById("students-feeling")!!}
-{{--    <h1 class="section-title">Feeling</h1>--}}
-{{--    <div class="section-content">--}}
-{{--        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="false">--}}
-{{--            <ol class="carousel-indicators">--}}
-{{--                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>--}}
-{{--                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>--}}
-{{--                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>--}}
-{{--            </ol>--}}
-{{--            <div class="carousel-inner">--}}
-{{--                <div class="carousel-item active first-item">--}}
-{{--                    <div class="display-flex-content">--}}
-{{--                        <div class="card col-3 student" data-student="2">--}}
-{{--                            <img src="{{asset("img/student_avatar/aaa.jpeg")}}" class="card-img-top" alt="...">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <h5 class="card-title">Bạn A, Hải Dương, Khóa thi năm 2015, đỗ thu khoa ĐH Tài Nguyên--}}
-{{--                                    Môi Trường</h5>--}}
-{{--                                <p class="card-text">“Được học ở AT, thật sự rất vui, rất nhiều kỷ niệm. Phải học ở đây--}}
-{{--                                    mới biết được nhiều thứ về môn địa lý mà chắc từ trước đến giờ mình chưa bao giờ để--}}
-{{--                                    ý đến, đặc biệt là cách vẽ biểu đồ chỉ có tại lớp của anh Tùng. hì hì. Học ở đây vừa--}}
-{{--                                    được khóc mà cũng được cười luôn. Còn vì sao khóc thì các bạn cứ học dần với anh--}}
-{{--                                    Tùng rồi biết nhé!”</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="card col-3 student active" data-student="1">--}}
-{{--                            <img src="{{asset("img/student_avatar/aaa.jpeg")}}" class="card-img-top" alt="...">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <h5 class="card-title">Bạn A, Hải Dương, Khóa thi năm 2015, đỗ thu khoa ĐH Tài Nguyên--}}
-{{--                                    Môi Trường</h5>--}}
-{{--                                <p class="card-text">“Được học ở AT, thật sự rất vui, rất nhiều kỷ niệm. Phải học ở đây--}}
-{{--                                    mới biết được nhiều thứ về môn địa lý mà chắc từ trước đến giờ mình chưa bao giờ để--}}
-{{--                                    ý đến, đặc biệt là cách vẽ biểu đồ chỉ có tại lớp của anh Tùng. hì hì. Học ở đây vừa--}}
-{{--                                    được khóc mà cũng được cười luôn. Còn vì sao khóc thì các bạn cứ học dần với anh--}}
-{{--                                    Tùng rồi biết nhé!”</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="card col-3 student" data-student="3">--}}
-{{--                            <img src="{{asset("img/student_avatar/aaa.jpeg")}}" class="card-img-top" alt="...">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <h5 class="card-title">Bạn A, Hải Dương, Khóa thi năm 2015, đỗ thu khoa ĐH Tài Nguyên--}}
-{{--                                    Môi Trường</h5>--}}
-{{--                                <p class="card-text">“Được học ở AT, thật sự rất vui, rất nhiều kỷ niệm. Phải học ở đây--}}
-{{--                                    mới biết được nhiều thứ về môn địa lý mà chắc từ trước đến giờ mình chưa bao giờ để--}}
-{{--                                    ý đến, đặc biệt là cách vẽ biểu đồ chỉ có tại lớp của anh Tùng. hì hì. Học ở đây vừa--}}
-{{--                                    được khóc mà cũng được cười luôn. Còn vì sao khóc thì các bạn cứ học dần với anh--}}
-{{--                                    Tùng rồi biết nhé!”</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="carousel-item">--}}
-{{--                    <div class="display-flex-content">--}}
-{{--                        <div class="card col-3 student">--}}
-{{--                            <img src="{{asset("img/student_avatar/aaa.jpeg")}}" class="card-img-top" alt="...">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <h5 class="card-title">Bạn A, Hải Dương, Khóa thi năm 2015, đỗ thu khoa ĐH Tài Nguyên--}}
-{{--                                    Môi Trường</h5>--}}
-{{--                                <p class="card-text">“Được học ở AT, thật sự rất vui, rất nhiều kỷ niệm. Phải học ở đây--}}
-{{--                                    mới biết được nhiều thứ về môn địa lý mà chắc từ trước đến giờ mình chưa bao giờ để--}}
-{{--                                    ý đến, đặc biệt là cách vẽ biểu đồ chỉ có tại lớp của anh Tùng. hì hì. Học ở đây vừa--}}
-{{--                                    được khóc mà cũng được cười luôn. Còn vì sao khóc thì các bạn cứ học dần với anh--}}
-{{--                                    Tùng rồi biết nhé!”</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="card col-3 student active">--}}
-{{--                            <img src="{{asset("img/student_avatar/aaa.jpeg")}}" class="card-img-top" alt="...">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <h5 class="card-title">Bạn A, Hải Dương, Khóa thi năm 2015, đỗ thu khoa ĐH Tài Nguyên--}}
-{{--                                    Môi Trường</h5>--}}
-{{--                                <p class="card-text">“Được học ở AT, thật sự rất vui, rất nhiều kỷ niệm. Phải học ở đây--}}
-{{--                                    mới biết được nhiều thứ về môn địa lý mà chắc từ trước đến giờ mình chưa bao giờ để--}}
-{{--                                    ý đến, đặc biệt là cách vẽ biểu đồ chỉ có tại lớp của anh Tùng. hì hì. Học ở đây vừa--}}
-{{--                                    được khóc mà cũng được cười luôn. Còn vì sao khóc thì các bạn cứ học dần với anh--}}
-{{--                                    Tùng rồi biết nhé!”</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="card col-3 student">--}}
-{{--                            <img src="{{asset("img/student_avatar/aaa.jpeg")}}" class="card-img-top" alt="...">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <h5 class="card-title">Bạn A, Hải Dương, Khóa thi năm 2015, đỗ thu khoa ĐH Tài Nguyên--}}
-{{--                                    Môi Trường</h5>--}}
-{{--                                <p class="card-text">“Được học ở AT, thật sự rất vui, rất nhiều kỷ niệm. Phải học ở đây--}}
-{{--                                    mới biết được nhiều thứ về môn địa lý mà chắc từ trước đến giờ mình chưa bao giờ để--}}
-{{--                                    ý đến, đặc biệt là cách vẽ biểu đồ chỉ có tại lớp của anh Tùng. hì hì. Học ở đây vừa--}}
-{{--                                    được khóc mà cũng được cười luôn. Còn vì sao khóc thì các bạn cứ học dần với anh--}}
-{{--                                    Tùng rồi biết nhé!”</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="carousel-item">--}}
-{{--                    <div class="display-flex-content">--}}
-{{--                        <div class="card col-3 student">--}}
-{{--                            <img src="{{asset("img/student_avatar/aaa.jpeg")}}" class="card-img-top" alt="...">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <h5 class="card-title">Bạn A, Hải Dương, Khóa thi năm 2015, đỗ thu khoa ĐH Tài Nguyên--}}
-{{--                                    Môi Trường</h5>--}}
-{{--                                <p class="card-text">“Được học ở AT, thật sự rất vui, rất nhiều kỷ niệm. Phải học ở đây--}}
-{{--                                    mới biết được nhiều thứ về môn địa lý mà chắc từ trước đến giờ mình chưa bao giờ để--}}
-{{--                                    ý đến, đặc biệt là cách vẽ biểu đồ chỉ có tại lớp của anh Tùng. hì hì. Học ở đây vừa--}}
-{{--                                    được khóc mà cũng được cười luôn. Còn vì sao khóc thì các bạn cứ học dần với anh--}}
-{{--                                    Tùng rồi biết nhé!”</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="card col-3 student active">--}}
-{{--                            <img src="{{asset("img/student_avatar/aaa.jpeg")}}" class="card-img-top" alt="...">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <h5 class="card-title">Bạn A, Hải Dương, Khóa thi năm 2015, đỗ thu khoa ĐH Tài Nguyên--}}
-{{--                                    Môi Trường</h5>--}}
-{{--                                <p class="card-text">“Được học ở AT, thật sự rất vui, rất nhiều kỷ niệm. Phải học ở đây--}}
-{{--                                    mới biết được nhiều thứ về môn địa lý mà chắc từ trước đến giờ mình chưa bao giờ để--}}
-{{--                                    ý đến, đặc biệt là cách vẽ biểu đồ chỉ có tại lớp của anh Tùng. hì hì. Học ở đây vừa--}}
-{{--                                    được khóc mà cũng được cười luôn. Còn vì sao khóc thì các bạn cứ học dần với anh--}}
-{{--                                    Tùng rồi biết nhé!”</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="card col-3 student">--}}
-{{--                            <img src="{{asset("img/student_avatar/aaa.jpeg")}}" class="card-img-top" alt="...">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <h5 class="card-title">Bạn A, Hải Dương, Khóa thi năm 2015, đỗ thu khoa ĐH Tài Nguyên--}}
-{{--                                    Môi Trường</h5>--}}
-{{--                                <p class="card-text">“Được học ở AT, thật sự rất vui, rất nhiều kỷ niệm. Phải học ở đây--}}
-{{--                                    mới biết được nhiều thứ về môn địa lý mà chắc từ trước đến giờ mình chưa bao giờ để--}}
-{{--                                    ý đến, đặc biệt là cách vẽ biểu đồ chỉ có tại lớp của anh Tùng. hì hì. Học ở đây vừa--}}
-{{--                                    được khóc mà cũng được cười luôn. Còn vì sao khóc thì các bạn cứ học dần với anh--}}
-{{--                                    Tùng rồi biết nhé!”</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">--}}
-{{--                <span class="carousel-control-prev-icon" aria-hidden="true"></span>--}}
-{{--                <span class="sr-only">Previous</span>--}}
-{{--            </a>--}}
-{{--            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">--}}
-{{--                <span class="carousel-control-next-icon" aria-hidden="true"></span>--}}
-{{--                <span class="sr-only">Next</span>--}}
-{{--            </a>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+    {{--<div class="landing-page-section-wrapper">--}}
+        {{--<h1 class="section-title" style="color: #fff">Feeling</h1>--}}
+        {{--<div class="section-content">--}}
+            {{--<div id="testimonial-slider" class="landing-testimonials">--}}
+                {{--<div class="testimonial">--}}
+                    {{--<div class="testimonial-content">--}}
+                        {{--<div class="pic">--}}
+                            {{--<img src="{{ asset('img/img-3.jpg')}}" alt="">--}}
+                        {{--</div>--}}
+                        {{--<h3 class="title">Williamson</h3>--}}
+                        {{--<span class="post">Web Developer</span>--}}
+                    {{--</div>--}}
+                    {{--<p class="description">--}}
+                        {{--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor tellus, efficitur ut tortor id, molestie egestas nibh. In blandit ex at erat vehicula molestie. Mauris vel volutpat nulla. Suspendisse lorem ex, congue at elit id, tincidunt tempor orci. Nullam nec augue ac tellus rhoncus tincidunt nec ut ligula. Praesent.--}}
+                    {{--</p>--}}
+                {{--</div>--}}
+
+                {{--<div class="testimonial">--}}
+                    {{--<div class="testimonial-content">--}}
+                        {{--<div class="pic">--}}
+                            {{--<img src="{{ asset('img/img-3.jpg')}}" alt="">--}}
+                        {{--</div>--}}
+                        {{--<h3 class="title">Williamson</h3>--}}
+                        {{--<span class="post">Web Developer</span>--}}
+                    {{--</div>--}}
+                    {{--<p class="description">--}}
+                        {{--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor tellus, efficitur ut tortor id, molestie egestas nibh. In blandit ex at erat vehicula molestie. Mauris vel volutpat nulla. Suspendisse lorem ex, congue at elit id, tincidunt tempor orci. Nullam nec augue ac tellus rhoncus tincidunt nec ut ligula. Praesent.--}}
+                    {{--</p>--}}
+                {{--</div>--}}
+
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 </section>
 <script type="text/javascript">
 
--- resources/views/landing-page/introduction/partials/hall_of_flame.blade.php
+++ resources/views/landing-page/introduction/partials/hall_of_flame.blade.php
@@ -1,39 +1,42 @@
 <section class="landing-page-section flame-section">
     {!!\Helper::getStaticBlockContentById("hall-of-flame")!!}
-{{--    <h2 class="section-title">PHÒNG TRUYỀN THỐNG</h2>--}}
-{{--    <div class="section-content">--}}
-{{--        <div class="section-table-head">--}}
-{{--            <ul>--}}
-{{--                <li class="th">STT</li>--}}
-{{--                <li>Họ Tên</li>--}}
-{{--                <li>Năm Thi</li>--}}
-{{--                <li>Số Điểm Môn Địa Lý</li>--}}
-{{--            </ul>--}}
-{{--        </div>--}}
-{{--        <table class="table">--}}
-{{--            <tbody>--}}
-{{--            <tr>--}}
-{{--                <td class="th"><a href="{{url("about/students#feeling_section")}}" data-student="1" class="student-sort">1</a>--}}
-{{--                </td>--}}
-{{--                <td><a href="{{url("about/students#feeling_section")}}" data-student="1" class="student-sort">Mark</a></td>--}}
-{{--                <td><a href="{{url("about/students#feeling_section")}}" data-student="1" class="student-sort">Otto</a></td>--}}
-{{--                <td><a href="{{url("about/students#feeling_section")}}" data-student="1" class="student-sort">@mdo</a></td>--}}
-{{--            </tr>--}}
-{{--            <tr>--}}
-{{--                    <td class="th"><a href="{{url("about/students#feeling_section")}}" data-student="2" class="student-sort">2</a>--}}
-{{--                    </td>--}}
-{{--                    <td><a href="{{url("about/students#feeling_section")}}" data-student="2" class="student-sort">Mark</a></td>--}}
-{{--                    <td><a href="{{url("about/students#feeling_section")}}" data-student="2" class="student-sort">Otto</a></td>--}}
-{{--                    <td><a href="{{url("about/students#feeling_section")}}" data-student="2" class="student-sort">@mdo</a></td>--}}
-{{--            </tr>--}}
-{{--            <tr>--}}
-{{--                <td class="th"><a href="{{url("about/studens#feeling_section")}}" data-student="3" class="student-sort">3</a>--}}
-{{--                </td>--}}
-{{--                <td><a href="{{url("about/students#feeling_section")}}" data-student="3" class="student-sort">Mark</a></td>--}}
-{{--                <td><a href="{{url("about/students#feeling_section")}}" data-student="3" class="student-sort">Otto</a></td>--}}
-{{--                <td><a href="{{url("about/students#feeling_section")}}" data-student="3" class="student-sort">@mdo</a></td>--}}
-{{--            </tr>--}}
-{{--            </tbody>--}}
-{{--        </table>--}}
-{{--    </div>--}}
+    {{--<div class="landing-page-section-wrapper">--}}
+
+        {{--<div class="section-content">--}}
+            {{--<div class="table-users">--}}
+                {{--<div class="header"><h2 class="section-title">PHÒNG TRUYỀN THỐNG</h2></div>--}}
+
+                {{--<table cellspacing="0">--}}
+                    {{--<tr>--}}
+                        {{--<th>STT</th>--}}
+                        {{--<th>Họ Tên</th>--}}
+                        {{--<th>Năm Thi</th>--}}
+                        {{--<th>Số Điểm Môn Địa Lý</th>--}}
+                    {{--</tr>--}}
+                    {{--<tr>--}}
+                        {{--<td class="th" data-title="STT"><a href="{{url("about/students#feeling_section")}}" data-student="1" class="student-sort">1</a>--}}
+                        {{--</td>--}}
+                        {{--<td data-title="Tên"><a href="{{url("about/students#feeling_section")}}"  data-student="1" class="student-sort">Mark</a></td>--}}
+                        {{--<td data-title="Năm"><a href="{{url("about/students#feeling_section")}}"  data-student="1" class="student-sort">Otto</a></td>--}}
+                        {{--<td data-title="Điểm"><a href="{{url("about/students#feeling_section")}}"  data-student="1" class="student-sort">@mdo</a></td>--}}
+                    {{--</tr>--}}
+                    {{--<tr>--}}
+                        {{--<td data-title="STT" class="th"><a href="{{url("about/students#feeling_section")}}"  data-student="2" class="student-sort">2</a>--}}
+                        {{--</td>--}}
+                        {{--<td data-title="Tên"><a href="{{url("about/students#feeling_section")}}"  data-student="2" class="student-sort">Mark</a></td>--}}
+                        {{--<td data-title="Năm"><a href="{{url("about/students#feeling_section")}}"  data-student="2" class="student-sort">Otto</a></td>--}}
+                        {{--<td data-title="Điểm"><a href="{{url("about/students#feeling_section")}}" data-student="2" class="student-sort">@mdo</a></td>--}}
+                    {{--</tr>--}}
+                    {{--<tr>--}}
+                        {{--<td  data-title="STT"  class="th"><a href="{{url("about/studens#feeling_section")}}" data-student="3" class="student-sort">3</a>--}}
+                        {{--</td>--}}
+                        {{--<td data-title="Tên"><a href="{{url("about/students#feeling_section")}}"  data-student="3" class="student-sort">Mark</a></td>--}}
+                        {{--<td data-title="Năm"><a href="{{url("about/students#feeling_section")}}"  data-student="3" class="student-sort">Otto</a></td>--}}
+                        {{--<td data-title="Điểm"><a href="{{url("about/students#feeling_section")}}"  data-student="3" class="student-sort">@mdo</a></td>--}}
+                    {{--</tr>--}}
+                {{--</table>--}}
+            {{--</div>--}}
+
+        {{--</div>--}}
+    {{--</div>--}}
 </section>
