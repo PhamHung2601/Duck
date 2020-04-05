df668f7b854f04d5319c1ff76fb99b9295286ec6
Trang <trang@azebiz.com>
2020-04-05 18:00:37 +0700

    landing courses/list/online/live-stream-overview

--- resources/views/landing-page/list/partials/online/live_stream_notice.blade.php
+++ resources/views/landing-page/list/partials/online/live_stream_notice.blade.php
@@ -1,43 +1,55 @@
-<section class="landing-page-section bg-success offline-course">
+<section class="landing-page-section bg-success text-light">
     {!!Helper::getStaticBlockContentById("online-1-block-5")!!}
-{{--    <h2 class="section-title">CÁCH HỌC NÀY CÓ GÌ THÚ VỊ?</h2>--}}
-{{--    <div class="section-content">--}}
-{{--        <ul>--}}
-{{--            <li><p><i class="fa fa-2x fa-location-arrow"> Có thể xem lại bài học bất cứ lúc nào, bất kì tại đâu, không hạn chế số lần, không sợ trùng lịch học môn khác</i></p></li>--}}
-{{--            <li><p><i class="fa fa-2x fa-location-arrow"> Livestream trực tiếp giúp em giải đáp nhanh chóng những điều còn vướng mắc. Có nhóm chat riêng để học sinh trao đổi bài, câu hỏi ngay khi cần</i></p></li>--}}
-{{--            <li><p><i class="fa fa-2x fa-location-arrow">  Tiết kiệm chi phí so với học trên lớp</i></p></li>--}}
-{{--            <li><p><i class="fa fa-2x fa-location-arrow">  Học chuyên sâu, tỉ mỉ. Các học trò mất gốc dễ dàng bắt đầu lại từ đầu - học trò khá, giỏi hiểu sâu, hiểu rõ bản chất vấn đề từ đó tư duy để làm các bài tập khó</i></p></li>--}}
-{{--        </ul>--}}
-{{--    </div>--}}
+    {{--<div class="landing-page-section-wrapper">--}}
+        {{--<h2 class="section-title">CÁCH HỌC NÀY CÓ GÌ THÚ VỊ?</h2>--}}
+        {{--<div class="section-content text-left text-light">--}}
+            {{--<ul>--}}
+                {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Có thể xem lại bài học bất cứ lúc nào, bất kì tại đâu, không hạn chế số lần, không sợ trùng lịch học môn khác</p></li>--}}
+                {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Livestream trực tiếp giúp em giải đáp nhanh chóng những điều còn vướng mắc. Có nhóm chat riêng để học sinh trao đổi bài, câu hỏi ngay khi cần</p></li>--}}
+                {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Tiết kiệm chi phí so với học trên lớp</p></li>--}}
+                {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Học chuyên sâu, tỉ mỉ. Các học trò mất gốc dễ dàng bắt đầu lại từ đầu - học trò khá, giỏi hiểu sâu, hiểu rõ bản chất vấn đề từ đó tư duy để làm các bài tập khó</p></li>--}}
+            {{--</ul>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 </section>
 <section class="landing-page-section bg-light">
     {!!Helper::getStaticBlockContentById("online-1-block-6")!!}
-{{--    <h2 class="section-title">NỘI QUY NHÀ ONG</h2>--}}
-{{--    <div class="section-content display-flex-content">--}}
-{{--        <div class="col-6">--}}
-{{--            <ul>--}}
-{{--                <li>Học sinh trong lớp được chia thành các nhóm nhỏ, 20 người/ nhóm</li>--}}
-{{--                <li>Mỗi nhóm do 1 Ong thợ phụ trách - có nhiệm vụ theo dõi, thúc đẩy việc học. Ong thợ có thể là giáo viên Địa lí hoặc học sinh cũ của thầy Tùng có điểm thi Địa lí 9+, Thủ khoa các trường</li>--}}
-{{--            </ul>--}}
-{{--        </div>--}}
-{{--        <div class="col-6">--}}
-{{--            <ul>--}}
-{{--                <li>Nhiệm vụ (bài tập) cần hoàn thiện sau 2 ngày đăng. 3 buổi không làm bài tập sẽ không được tham gia lớp. Sau khi hoàn thành bài tập còn thiếu mới được trở lại</li>--}}
-{{--                <li>Không chia sẻ tài liệu, bài tập của lớp ra bên ngoài. Trường hợp vi phạm sẽ không được tham gia lớp</li>--}}
-{{--            </ul>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+    {{--<div class="landing-page-section-wrapper">--}}
+        {{--<h2 class="section-title">NỘI QUY NHÀ ONG</h2>--}}
+        {{--<div class="section-content text-left row">--}}
+            {{--<div class="col-sm-12 col-md-6 col-lg-6">--}}
+                {{--<ul>--}}
+                    {{--<li>Học sinh trong lớp được chia thành các nhóm nhỏ, 20 người/ nhóm</li>--}}
+                    {{--<li>Mỗi nhóm do 1 Ong thợ phụ trách - có nhiệm vụ theo dõi, thúc đẩy việc học. Ong thợ có thể là--}}
+                        {{--giáo viên Địa lí hoặc học sinh cũ của thầy Tùng có điểm thi Địa lí 9+, Thủ khoa các trường--}}
+                    {{--</li>--}}
+                {{--</ul>--}}
+            {{--</div>--}}
+            {{--<div class="col-sm-12 col-md-6 col-lg-6">--}}
+                {{--<ul>--}}
+                    {{--<li>Nhiệm vụ (bài tập) cần hoàn thiện sau 2 ngày đăng. 3 buổi không làm bài tập sẽ không được tham--}}
+                        {{--gia lớp. Sau khi hoàn thành bài tập còn thiếu mới được trở lại--}}
+                    {{--</li>--}}
+                    {{--<li>Không chia sẻ tài liệu, bài tập của lớp ra bên ngoài. Trường hợp vi phạm sẽ không được tham gia--}}
+                        {{--lớp--}}
+                    {{--</li>--}}
+                {{--</ul>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 </section>
 <section class="landing-page-section">
     {!!Helper::getStaticBlockContentById("online-1-block-7")!!}
-{{--    <h2 class="section-title">CAM KẾT</h2>--}}
-{{--    <div class="section-content">--}}
-{{--        <ul>--}}
-{{--            <li><p><i class="fa fa-2x fa-location-arrow"> Giảng dạy nhiệt tình</i></p></li>--}}
-{{--            <li><p><i class="fa fa-2x fa-location-arrow">  Chắc chắn hiểu bài, tiến bộ rõ rệt</i></p></li>--}}
-{{--            <li><p><i class="fa fa-2x fa-location-arrow">  Yêu thích học môn Địa lí hơn</i></p></li>--}}
-{{--            <li><p><i class="fa fa-2x fa-location-arrow">  Hơn 90% học sinh theo học có điểm số cao hơn mong muốn</i></p></li>--}}
-{{--            <li><p><i class="fa fa-2x fa-location-arrow">  Được nhận sự hỗ trợ học tập chưa nơi học thêm nào có</i></p></li>--}}
-{{--        </ul>--}}
-{{--    </div>--}}
+    {{--<div class="landing-page-section-wrapper">--}}
+        {{--<h2 class="section-title">CAM KẾT</h2>--}}
+        {{--<div class="section-content text-left">--}}
+            {{--<ul>--}}
+                {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Giảng dạy nhiệt tình</p></li>--}}
+                {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Chắc chắn hiểu bài, tiến bộ rõ rệt</p></li>--}}
+                {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Yêu thích học môn Địa lí hơn</p></li>--}}
+                {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Hơn 90% học sinh theo học có điểm số cao hơn mong muốn</p></li>--}}
+                {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Được nhận sự hỗ trợ học tập chưa nơi học thêm nào có</p></li>--}}
+            {{--</ul>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 </section>
