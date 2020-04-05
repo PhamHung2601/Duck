788d6b1dfb54b7341b4b70d9bde428051427cfb4
Trang <trang@azebiz.com>
2020-04-05 10:29:23 +0700

    landing courses/list/offline #2

--- resources/views/landing-page/list/partials/offline/course_list.blade.php
+++ resources/views/landing-page/list/partials/offline/course_list.blade.php
@@ -1,29 +1,29 @@
 <section class="landing-page-section offline-course">
-{{--{!!Helper::getStaticBlockContentById("offline-block-1")!!}--}}
-    <div class="landing-page-section-wrapper">
-        <div class="row">
-            <div class="col-sm-12 col-md-6 col-lg-6">
-                <div class="image-wrapper">
-                    <div class="image-container">
-                        <img src="{{ asset('img/hiclipart.com.png')}}" alt="off-img">
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-6">
-                <h2 class="section-title">KHOÁ HỌC DÀNH CHO AI?</h2>
-                <div class="section-content">
-                    <ul>
-                        <li><p><i class="fa fa-2x fa-location-arrow"></i>Tất cả các bạn có ý định dự thi THPT Quốc gia năm 2020</p></li>
-                        <li><p><i class="fa fa-2x fa-location-arrow"></i>Được thiết kế phù hợp với cả những bạn đã mất gốc, chán ghét môn Địa lí; những học sinh thi lại cần hiểu sâu, hiểu chắc vấn đề</p></li>
-                        <li><p><i class="fa fa-2x fa-location-arrow"></i> Mục tiêu từ 8 - 10 điểm.</p></li>
-                    </ul>
-                </div>
-                <div class="box-action">
-                    <a class="landing-button" href="#">Tao muon mua luon</a>
-                </div>
-            </div>
-        </div>
-    </div>
+{!!Helper::getStaticBlockContentById("offline-block-1")!!}
+    {{--<div class="landing-page-section-wrapper">--}}
+        {{--<div class="row">--}}
+            {{--<div class="col-sm-12 col-md-6 col-lg-6">--}}
+                {{--<div class="image-wrapper">--}}
+                    {{--<div class="image-container">--}}
+                        {{--<img src="{{ asset('img/hiclipart.com.png')}}" alt="off-img">--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="col-sm-12 col-md-6 col-lg-6">--}}
+                {{--<h2 class="section-title">KHOÁ HỌC DÀNH CHO AI?</h2>--}}
+                {{--<div class="section-content">--}}
+                    {{--<ul>--}}
+                        {{--<li><p><i class="fa fa-2x fa-location-arrow"></i>Tất cả các bạn có ý định dự thi THPT Quốc gia năm 2020</p></li>--}}
+                        {{--<li><p><i class="fa fa-2x fa-location-arrow"></i>Được thiết kế phù hợp với cả những bạn đã mất gốc, chán ghét môn Địa lí; những học sinh thi lại cần hiểu sâu, hiểu chắc vấn đề</p></li>--}}
+                        {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Mục tiêu từ 8 - 10 điểm.</p></li>--}}
+                    {{--</ul>--}}
+                {{--</div>--}}
+                {{--<div class="box-action">--}}
+                    {{--<a class="landing-button" href="#">Tao muon mua luon</a>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 </section>
 <section class="landing-page-section course-register">
     <div class="landing-page-section-wrapper">
@@ -32,18 +32,18 @@
                 {{ session('success') }}
             </div>
         @endif
-        {{--    {!!Helper::getStaticBlockContentById("offline-block-2-title")!!}--}}
-        <h2 class="section-title">TẠI SAO KHÔNG THỂ BỎ LỠ KHOÁ HỌC NÀY?</h2>
+            {!!Helper::getStaticBlockContentById("offline-block-2-title")!!}
+        {{--<h2 class="section-title">TẠI SAO KHÔNG THỂ BỎ LỠ KHOÁ HỌC NÀY?</h2>--}}
         <div class="section-content">
-            {{--        {!!Helper::getStaticBlockContentById("offline-block-2-content")!!}--}}
             <div class="row">
                 <div class="col-sm-12 col-md-7 col-lg-7">
-                    <ul>
-                        <h3>Với lộ trình:  Đào kiến thức - Thạo kĩ năng - Luyện đề chuyên sâu, các em sẽ được</h3>
-                        <li><p><i class="fa fa-play"> Học lại toàn bộ kiến thức lí thuyết 11 + 12, biểu đồ, atlat, bảng số liệu</i></p></li>
-                        <li><p><i class="fa fa-play"> Bài tập củng cố và  luyện đề chuyên sâu  nhằm giúp làm quen với các câu hỏi, dạng đề thường gặp</i></p></li>
-                        <li><p><i class="fa fa-play"> Hoàn thiện kĩ năng, phản xạ trong việc xử lí nhanh câu hỏi trắc nghiệm</i></p></li>
-                    </ul>
+                    {!!Helper::getStaticBlockContentById("offline-block-2-content")!!}
+                    {{--<ul>--}}
+                        {{--<h3>Với lộ trình:  Đào kiến thức - Thạo kĩ năng - Luyện đề chuyên sâu, các em sẽ được</h3>--}}
+                        {{--<li><p><i class="fa fa-play"> Học lại toàn bộ kiến thức lí thuyết 11 + 12, biểu đồ, atlat, bảng số liệu</i></p></li>--}}
+                        {{--<li><p><i class="fa fa-play"> Bài tập củng cố và  luyện đề chuyên sâu  nhằm giúp làm quen với các câu hỏi, dạng đề thường gặp</i></p></li>--}}
+                        {{--<li><p><i class="fa fa-play"> Hoàn thiện kĩ năng, phản xạ trong việc xử lí nhanh câu hỏi trắc nghiệm</i></p></li>--}}
+                    {{--</ul>--}}
                 </div>
                 <div class="join-course col-sm-12 col-md-5 col-lg-5">
                     <form class="form-register-course" action="{{ route('course.register') }}" method="POST">
@@ -64,13 +64,13 @@
                         @error('address')
                         <small class="form-text text-muted">{{ $message }}</small>
                         @enderror
-                        <h3>Học phí khoá học: 80.000 VNĐ/buổi. Thời gian: 2 tiếng</h3>
-                        <div class="box-action">
-                            <button type="submit" id="register-course-button" class="btn btn-success register-course-button landing-button">Dang Ky
-                                Ngay
-                            </button>
-                        </div>
-                        {{--                {!!Helper::getStaticBlockContentById("offline-block-2-button")!!}--}}
+                        {{--<h3>Học phí khoá học: 80.000 VNĐ/buổi. Thời gian: 2 tiếng</h3>--}}
+                        {{--<div class="box-action">--}}
+                            {{--<button type="submit" id="register-course-button" class="btn btn-success register-course-button landing-button">Dang Ky--}}
+                                {{--Ngay--}}
+                            {{--</button>--}}
+                        {{--</div>--}}
+                                        {!!Helper::getStaticBlockContentById("offline-block-2-button")!!}
                     </form>
                 </div>
             </div>
@@ -78,17 +78,17 @@
     </div>
 </section>
 <section class="landing-page-section bg-yellow our-commit">
-{{--    {!!Helper::getStaticBlockContentById("offline-block-3")!!}--}}
-    <div class="landing-page-section-wrapper">
-        <h2 class="section-title">CAM KẾT</h2>
-        <div class="section-content">
-            <ul>
-                    <li><p><i class="fa fa-2x fa-location-arrow"></i> Giảng dạy nhiệt tình</p></li>
-                <li><p><i class="fa fa-2x fa-location-arrow"></i> Chắc chắn hiểu bài, tiến bộ rõ rệt</p></li>
-                <li><p><i class="fa fa-2x fa-location-arrow"></i> Yêu thích học môn Địa lí hơn</p></li>
-                <li><p><i class="fa fa-2x fa-location-arrow"></i> Hơn 90% học sinh theo học có điểm số cao hơn mong muốn</p></li>
-                <li><p><i class="fa fa-2x fa-location-arrow"></i> Được nhận sự hỗ trợ học tập chưa nơi học thêm nào có</p></li>
-            </ul>
-        </div>
-    </div>
+    {!!Helper::getStaticBlockContentById("offline-block-3")!!}
+    {{--<div class="landing-page-section-wrapper">--}}
+        {{--<h2 class="section-title">CAM KẾT</h2>--}}
+        {{--<div class="section-content">--}}
+            {{--<ul>--}}
+                    {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Giảng dạy nhiệt tình</p></li>--}}
+                {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Chắc chắn hiểu bài, tiến bộ rõ rệt</p></li>--}}
+                {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Yêu thích học môn Địa lí hơn</p></li>--}}
+                {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Hơn 90% học sinh theo học có điểm số cao hơn mong muốn</p></li>--}}
+                {{--<li><p><i class="fa fa-2x fa-location-arrow"></i> Được nhận sự hỗ trợ học tập chưa nơi học thêm nào có</p></li>--}}
+            {{--</ul>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 </section>
