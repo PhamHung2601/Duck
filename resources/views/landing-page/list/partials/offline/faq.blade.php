7066f692fc27f2e3921c7b7c7f9fad77644c23b8
Trang <trang@azebiz.com>
2020-04-05 11:15:04 +0700

    landing courses/list/offline #3

--- resources/views/landing-page/list/partials/offline/faq.blade.php
+++ resources/views/landing-page/list/partials/offline/faq.blade.php
@@ -1,37 +1,58 @@
-<section class="landing-page-section bg-info">
+<section class="landing-page-section landing-content">
     {!!Helper::getStaticBlockContentById("offline-block-5")!!}
-{{--    <h2 class="section-title">Thắc Mắc?</h2>--}}
-{{--    <div class="section-content row-content">--}}
-{{--        <div class="row">--}}
-{{--            <div class="col-5">--}}
-{{--                <h3 class="title"><i class="fa fa-question-circle"> Địa điểm học tại đâu?</i></h3>--}}
-{{--                <div class="content">Tổ 8, thị trấn Quang Minh, Mê Linh, Hà Nội. (Gần Siêu thị Mê Linh Plaza). Bạn có--}}
-{{--                    thể đi xe bus số 07, 56, 58, hoặc 64 để qua đây nhé!--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="col-2"></div>--}}
-{{--            <div class="col-5">--}}
-{{--                <h3 class="title"><i class="fa fa-question-circle"> Mình muốn học toàn bộ kiến thức thì khoảng thời gian--}}
-{{--                        nào luyện thi thì phù hợp?</i></h3>--}}
-{{--                <div class="content">Khoảng thời gian tốt nhất là hè trước năm thi. Bạn sẽ có cơ hội học tỉ mỉ từng nội--}}
-{{--                    dung kiến thức. Học càng sớm càng đỡ vất vả về sau--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="row">--}}
-{{--            <div class="col-5">--}}
-{{--                <h3 class="title"><i class="fa fa-question-circle"> Lớp học có giới hạn số người học không?</i></h3>--}}
-{{--                <div class="content">Các khóa học của AT đều không quá 10 học viên một lớp. Ngoài ra sẽ có một số lớp--}}
-{{--                    đặc biệt chỉ có 2 học viên một lớp. AT không tổ chức các khóa học 1 – 1 (1 người học – 1 người dạy)--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="col-2"></div>--}}
-{{--            <div class="col-5">--}}
-{{--                <h3 class="title"><i class="fa fa-question-circle"> Làm cách nào để theo dõi thông tin về những khóa học--}}
-{{--                        mới?</i></h3>--}}
-{{--                <div class="content">Mời bạn theo dõi qua <a href="http://www.facebook.com/dialy123">facebook này.</a>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+    {{--<div class="section-6 section-box bg-grey">--}}
+        {{--<div class="landing-page-section-wrapper" style="padding: 0 15px;">--}}
+            {{--<h2 class="title-box">BẠN SẼ BỎ LỠ GÌ NẾU KHÔNG THAM GIA?</h2>--}}
+            {{--<div class="box-content">--}}
+                {{--<div class="row">--}}
+                    {{--<div class="col-sm-12 col-md-6">--}}
+                        {{--<div class="box-item">--}}
+                            {{--<div class="img-box">--}}
+                                {{--<img src="{{ asset('img/support-icon.png')}}" alt="support-icon"/>--}}
+                            {{--</div>--}}
+                            {{--<div class="text">--}}
+                                {{--<strong>Địa điểm học tại đâu?</strong>--}}
+                                {{--<p>Tổ 8, thị trấn Quang Minh, Mê Linh, Hà Nội. (Gần Siêu thị Mê Linh Plaza). Bạn có thể đi xe bus số 07, 56, 58, hoặc 64 để qua đây nhé!</p>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="col-sm-12 col-md-6">--}}
+                        {{--<div class="box-item">--}}
+                            {{--<div class="img-box">--}}
+                                {{--<img src="{{ asset('img/support-icon.png')}}" alt="support-icon"/>--}}
+                            {{--</div>--}}
+                            {{--<div class="text">--}}
+                                {{--<strong>Mình muốn học toàn bộ kiến thức thì khoảng thời gian nào luyện thi thì phù hợp?</strong>--}}
+                                {{--<p>Khoảng thời gian tốt nhất là hè trước năm thi. Bạn sẽ có cơ hội học tỉ mỉ từng nội dung kiến thức. Học càng sớm càng đỡ vất vả về sau</p>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+                {{--<div class="row">--}}
+                    {{--<div class="col-sm-12 col-md-6">--}}
+                        {{--<div class="box-item">--}}
+                            {{--<div class="img-box">--}}
+                                {{--<img src="{{ asset('img/support-icon.png')}}" alt="support-icon"/>--}}
+                            {{--</div>--}}
+                            {{--<div class="text">--}}
+                                {{--<strong>Lớp học có giới hạn số người học không?</strong>--}}
+                                {{--<p>Các khóa học của AT đều không quá 10 học viên một lớp. Ngoài ra sẽ có một số lớp đặc biệt chỉ có 2 học viên một lớp. AT không tổ chức các khóa học 1 – 1 (1 người học – 1 người dạy)</p>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="col-sm-12 col-md-6">--}}
+                        {{--<div class="box-item">--}}
+                            {{--<div class="img-box">--}}
+                                {{--<img src="{{ asset('img/support-icon.png')}}" alt="support-icon"/>--}}
+                            {{--</div>--}}
+                            {{--<div class="text">--}}
+                                {{--<strong>Làm cách nào để theo dõi thông tin về những khóa học mới?</strong>--}}
+                                {{--<p>Mời bạn theo dõi qua <a href="http://www.facebook.com/dialy123">facebook này.</a></p>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 </section>
