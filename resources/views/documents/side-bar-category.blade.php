9c026a996ae8073f25323650c4ec452d127e700a
Sam <sam@tigren.com>
2020-04-11 11:54:59 +0700

    fix some code.

--- resources/views/documents/side-bar-category.blade.php
+++ resources/views/documents/side-bar-category.blade.php
@@ -1,6 +1,6 @@
 <ul>
     @foreach(\Helper::getDocumentsCategory() as $key => $cat)
-        <li @if($key == $category) class="active" @endif >
+        <li @if(!empty($category) && $key == $category) class="active" @endif >
             <a href="{{route('document.by.category',[$key])}}">{{$cat}}</a>
         </li>
     @endforeach
