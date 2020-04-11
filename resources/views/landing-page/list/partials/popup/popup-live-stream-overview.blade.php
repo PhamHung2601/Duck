df67bcb1c0f5c12731bc6163b450e1343fa2dfbc
Sam <sam@tigren.com>
2020-04-11 12:11:07 +0700

    update popup.

---
+++ resources/views/landing-page/list/partials/popup/popup-live-stream-overview.blade.php
@@ -0,0 +1,24 @@
+<!-- The Modal -->
+<div class="modal" id="popup-register-live-stream-overview-success">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Thông báo</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal body -->
+            <div class="modal-body">
+                Cảm ơn đã mua khoá học vui lòng chuyển vào vào stk ... sau khi chuyển khoản bạn nhắn tin gửi hoá đơn cho chị địa lí nhé.
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success" data-dismiss="modal">Đóng</button>
+            </div>
+
+        </div>
+    </div>
+</div>
