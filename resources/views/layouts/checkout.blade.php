08ae76c2e48b623381c5de36d903cd6519cc942f
Trang <trang@azebiz.com>
2020-04-11 16:26:03 +0700

    responsive checkout

--- resources/views/layouts/checkout.blade.php
+++ resources/views/layouts/checkout.blade.php
@@ -2,13 +2,11 @@
 @section('pageTitle', 'Checkout')
 @section('content')
     <section>
-        <div class="container">
-            <div class="page-checkout">
+        <div class="page-checkout">
                 <form action="{{ url('/checkout/submit') }}" method="post">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    <div class="col-sm-12 clearfix">
-                        <div class="container">
-                            <div class="bill-to col-sm-12 clearfix">
+                    <div class="row">
+                        <div class="bill-to col-sm-12 clearfix">
                                 <p><h4>Thông tin khách hàng</h4></p>
                                 @if(count($errors) >0)
                                     <ul>
@@ -45,7 +43,7 @@
                                               rows="7"></textarea>
                                 </div>
                             </div>
-                            <div class="payment-method col-sm-12 clearfix">
+                        <div class="payment-method col-sm-12 clearfix">
                                 <p><h4>Phương thức thanh toán</h4></p>
                                 <div class="input-field">
                                     <input id="cod" name="payment_method" value="cashondelivery" type="radio">
@@ -56,82 +54,80 @@
                                     <label for="banktransfer">Chuyển khoản</label>
                                 </div>
                             </div>
-                        </div>
                     </div>
-                    <div class="col-sm-12">
-                        <section id="cart_items">
-                            <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <section id="cart_items">
                                 <div class="table-responsive cart_info">
-                                    <table class="table table-condensed">
-                                        <thead class="thead-light">
-                                        <tr class="cart_menu">
-                                            <th class="description">Tên sản phẩm</th>
-                                            <th class="price">Giá</th>
-                                            <th class="quantity">Số lượng</th>
-                                            <th class="total">Tổng</th>
-                                            <th></th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        @if(count($cart))
-                                            @foreach($cart as $item)
-                                                <tr>
-                                                    <td class="cart_description">
-                                                        <h6><a href="">{{ $item->name }}</a></h6>
-                                                    </td>
-                                                    <td class="cart_price">
-                                                        <p>{{ number_format($item->price)}} VNĐ</p>
-                                                    </td>
-                                                    <td class="cart_quantity">
-                                                        {{ $item->qty }}
-                                                    </td>
-                                                    <td class="cart_total">
-                                                        <p class="cart_total_price">{{ number_format($item->subtotal)}}
-                                                            VNĐ</p>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                            <tr>
-                                                <td colspan="2">&nbsp;
-                                                    <span>
+                                        <table class="table table-condensed">
+                                            <thead class="thead-light">
+                                            <tr class="cart_menu">
+                                                <th class="description">Sản phẩm</th>
+                                                <th class="price">Giá</th>
+                                                <th class="quantity">Số lượng</th>
+                                                <th class="total">Tổng</th>
+                                                <th></th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            @if(count($cart))
+                                                @foreach($cart as $item)
+                                                    <tr>
+                                                        <td class="cart_description" data-title="Tên sản phẩm">
+                                                            <h6><a href="">{{ $item->name }}</a></h6>
+                                                        </td>
+                                                        <td class="cart_price" data-title="Giá">
+                                                            <p>{{ number_format($item->price)}} VNĐ</p>
+                                                        </td>
+                                                        <td class="cart_quantity" data-title="Số lượng">
+                                                            {{ $item->qty }}
+                                                        </td>
+                                                        <td class="cart_total" data-title="Tổng">
+                                                            <p class="cart_total_price">{{ number_format($item->subtotal)}}
+                                                                VNĐ</p>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                                <tr class="summary">
+                                                    <td colspan="2">&nbsp;
+                                                        <span>
                                             <a class="btn btn-default update" href="{{ url('cart')}}">Quay về giỏ
                                                 hàng</a>
                                             </span>
 
-                                                </td>
-                                                <td colspan="2" class="grand-total">
-                                                    @if ($shippingFee > 0)
-                                                    <p style="text-align: center">
-                                                        <span>Giá vận chuyển : </span><span class="price">{{ $shippingFee }} VNĐ</span>
-                                                    </p>
-                                                    @endIf
-                                                    <p style="text-align: center">
-                                                        <span>Tổng : </span><span class="price">{{ $total }} VNĐ</span>
-                                                    </p>
-                                                    <button type="submit" class="btn btn-danger check_out">
-                                                        Gửi đơn hàng
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                        @else
-                                            <tr>
-                                                <td>You have no items in the shopping cart</td>
-                                            </tr>
-                                            <tr>
-                                                <td colspan="4">&nbsp;
-                                                    <a class="btn btn-default update" href="{{ url('/')}}">Mua hàng</a>
-                                                </td>
-                                            </tr>
-                                        @endif
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </section>
-                        <!--/#cart_items-->
+                                                    </td>
+                                                    <td colspan="2" class="grand-total">
+                                                        @if ($shippingFee > 0)
+                                                            <p style="text-align: center">
+                                                                <span>Giá vận chuyển : </span><span class="price">{{ $shippingFee }} VNĐ</span>
+                                                            </p>
+                                                        @endIf
+                                                        <p style="text-align: center">
+                                                            <span>Tổng : </span><span class="price">{{ $total }} VNĐ</span>
+                                                        </p>
+                                                        <button type="submit" class="btn btn-danger check_out">
+                                                            Gửi đơn hàng
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            @else
+                                                <tr>
+                                                    <td>You have no items in the shopping cart</td>
+                                                </tr>
+                                                <tr>
+                                                    <td colspan="4">&nbsp;
+                                                        <a class="btn btn-default update" href="{{ url('/')}}">Mua hàng</a>
+                                                    </td>
+                                                </tr>
+                                            @endif
+                                            </tbody>
+                                        </table>
+                                    </div>
+                            </section>
+                            <!--/#cart_items-->
+                        </div>
                     </div>
                 </form>
             </div>
-        </div>
     </section>
 @endsection
