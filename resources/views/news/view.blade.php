0e0099547b53a14d83170a99ad5f44218cd7b810
Loky <doankhachung.bk@gmail.com>
2020-04-11 08:56:04 +0700

    remove facebook share news page

--- resources/views/news/view.blade.php
+++ resources/views/news/view.blade.php
@@ -24,11 +24,6 @@
             </div>
         </div>
     </div>
-    <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count"
-         data-size="small"><a target="_blank"
-                              href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"
-                              class="fb-xfbml-parse-ignore">Chia sẻ</a></div>
-    </div>
 @endsection
 @section('content-bottom')
     @include('static-block.content-bottom')
