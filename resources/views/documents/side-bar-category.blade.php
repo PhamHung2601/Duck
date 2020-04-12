d27838eec45364893aa8fc01ee26b8fa64223b9a
congam <anmanhcong@gmail.com>
2020-04-12 10:17:22 +0700

    style man document

--- resources/views/documents/side-bar-category.blade.php
+++ resources/views/documents/side-bar-category.blade.php
@@ -1,7 +1,7 @@
-<ul>
+<ul class="list-group">
     @foreach(\Helper::getDocumentsCategory() as $key => $cat)
-        <li @if(!empty($category) && $key == $category) class="active" @endif class="document-sidebar">
-            <a class="document-cate-name" href="{{route('document.by.category',[$key])}}"> > {{$cat}}</a>
+        <li @if(!empty($category) && $key == $category) class="active" @endif class="document-sidebarlist-group-item btn btn-primary">
+            <a class="document-cate-name text-white" href="{{route('document.by.category',[$key])}}"> {{$cat}}</a>
         </li>
     @endforeach
 </ul>
