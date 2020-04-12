ea781f02270d9df552ad5eebae95083857f32bec
ngan <ngan@azebiz.com>
2020-04-12 21:46:09 +0700

    Tau commit

--- resources/views/landing-page/introduction/partials/hiring.blade.php
+++ resources/views/landing-page/introduction/partials/hiring.blade.php
@@ -1,17 +1,18 @@
 <section class="landing-page-section hiring">
-    {!! \Helper::getStaticBlockContentById("hiring-") !!}
+    {!! \Helper::getStaticBlockContentById("hiring-page-header") !!}
+    {!! \Helper::getStaticBlockContentById("hiring-page-content") !!}
     {{--<div class="hirring-banner">--}}
-        {{--<div class="landing-page-section-wrapper">--}}
-        {{--<h1 class="section-title">Tuyen Dung</h1>--}}
-        {{--</div>--}}
+{{--    <div class="landing-page-section-wrapper">--}}
+    {{--<h1 class="section-title">Tuyen Dung</h1>--}}
+    {{--</div>--}}
     {{--</div>--}}
 
     {{--<div class="landing-page-section-wrapper hirring-content">--}}
-            {{--<div class="section-content">--}}
-                {{--<p>Step Up tin rằng mỗi người đều có những tiềm năng vô hạn để trở nên giỏi giang. Vấn đề là họ không áp dụng--}}
-                    {{--đúng phương pháp để việc học hiệu quả hơn. Vì vậy Step Up mong muốn giúp cho những người gặp khó khăn trong--}}
-                    {{--việc học hành nói chung và học Tiếng Anh nói riêng được tiếp cận các phương pháp, kinh nghiệm học tiếng Anh--}}
-                    {{--thông minh để trở nên giỏi thực sự.</p>--}}
-            {{--</div>--}}
-        {{--</div>--}}
+    {{--<div class="section-content">--}}
+    {{--<p>Step Up tin rằng mỗi người đều có những tiềm năng vô hạn để trở nên giỏi giang. Vấn đề là họ không áp dụng--}}
+    {{--đúng phương pháp để việc học hiệu quả hơn. Vì vậy Step Up mong muốn giúp cho những người gặp khó khăn trong--}}
+    {{--việc học hành nói chung và học Tiếng Anh nói riêng được tiếp cận các phương pháp, kinh nghiệm học tiếng Anh--}}
+    {{--thông minh để trở nên giỏi thực sự.</p>--}}
+    {{--</div>--}}
+    {{--</div>--}}
 </section>
--- resources/views/layouts/block/footer.blade.php
+++ resources/views/layouts/block/footer.blade.php
@@ -1,30 +1,31 @@
 <footer class="footer-landing bg-light">
     <div class="landing-page-section-wrapper">
         <div class="row">
-            <div class="col-sm-12 col-md-1 col-lg-1"></div>
-            <div class="col-sm-12 col-md-4 col-lg-4" id="about-section">
-                <h3>Mr.Tung's Geography</h3>
-                <ul>
-                    <li><i class="fa fa-map-marker"></i> Địa chỉ: Tầng 5, nhà A24 ngõ 120 Hoàng Quốc Việt, P.Cổ Nhuế, Q.Cầu Giấy, Hà Nội</li>
-                    <li><i class="fa fa-phone"></i> SĐT: </li>
-                    <li><i class="fa fa-envelope"></i> Email: dialyat2013@gmail.com</li>
-                    <li><i class="fa fa-facebook"></i> Facebook: </li>
-                    <li><i class="fa fa-youtube-play"></i> Youtube: </li>
-                </ul>
-            </div>
-            <div class="col-sm-12 col-md-4 col-lg-4" id="service-section">
-                <h3>Customer Service</h3>
-                <ul>
-                    <li>Địa chỉ: Tầng 5, nhà A24 ngõ 120 Hoàng Quốc Việt, P.Cổ Nhuế, Q.Cầu Giấy, Hà Nội</li>
-                    <li>SĐT: </li>
-                    <li>Email: dialyat2013@gmail.com</li>
-                    <li>Facebook:</li>
-                    <li>Youtube:</li>
-                </ul>
-            </div>
-            <div class="col-sm-12 col-md-3 col-lg-3" id="footer-logo">
-                <img src="">
-            </div>
+            {!! \Helper::getStaticBlockContentById("footer") !!}
+{{--            <div class="col-sm-12 col-md-1 col-lg-1"></div>--}}
+{{--            <div class="col-sm-12 col-md-4 col-lg-4" id="about-section">--}}
+{{--                <h3>Mr.Tung's Geography</h3>--}}
+{{--                <ul>--}}
+{{--                    <li><i class="fa fa-map-marker"></i> Địa chỉ: Tầng 5, nhà A24 ngõ 120 Hoàng Quốc Việt, P.Cổ Nhuế, Q.Cầu Giấy, Hà Nội</li>--}}
+{{--                    <li><i class="fa fa-phone"></i> SĐT: </li>--}}
+{{--                    <li><i class="fa fa-envelope"></i> Email: dialyat2013@gmail.com</li>--}}
+{{--                    <li><i class="fa fa-facebook"></i> Facebook: </li>--}}
+{{--                    <li><i class="fa fa-youtube-play"></i> Youtube: </li>--}}
+{{--                </ul>--}}
+{{--            </div>--}}
+{{--            <div class="col-sm-12 col-md-4 col-lg-4" id="service-section">--}}
+{{--                <h3>Customer Service</h3>--}}
+{{--                <ul>--}}
+{{--                    <li>Địa chỉ: Tầng 5, nhà A24 ngõ 120 Hoàng Quốc Việt, P.Cổ Nhuế, Q.Cầu Giấy, Hà Nội</li>--}}
+{{--                    <li>SĐT: </li>--}}
+{{--                    <li>Email: dialyat2013@gmail.com</li>--}}
+{{--                    <li>Facebook:</li>--}}
+{{--                    <li>Youtube:</li>--}}
+{{--                </ul>--}}
+{{--            </div>--}}
+{{--            <div class="col-sm-12 col-md-3 col-lg-3" id="footer-logo">--}}
+{{--                <img src="">--}}
+{{--            </div>--}}
         </div>
     </div>
 </footer>
