09693eef63065442e7869693033ce19564d9a9be
charlie_bui <charlie@tigren.com>
2020-04-17 00:57:43 +0700

    facebook chat

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -33,21 +33,6 @@
 </head>
 
 <body>
-<!-- Start of LiveChat (www.livechatinc.com) code -->
-<script type="text/javascript">
-    window.__lc = window.__lc || {};
-    window.__lc.license = 11837022;
-    (function() {
-        var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
-        lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
-    })();
-</script>
-<noscript>
-    <a href="https://www.livechatinc.com/chat-with/11837022/" rel="nofollow">Chat with us</a>,
-    powered by <a href="https://www.livechatinc.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a>
-</noscript>
-<!-- End of LiveChat code -->
 
 @include('home-page.partials.header')
 @yield('content-top')
@@ -63,6 +48,30 @@
 @include('home-page.partials.footer')
 
 @include('home-page.partials.scripts')
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v6.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+     attribution=setup_tool
+     page_id="110310680466394"
+     theme_color="#0c958f">
+</div>
 </body>
 
 </html>
