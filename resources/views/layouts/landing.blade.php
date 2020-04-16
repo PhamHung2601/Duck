a6b7729643b663b840088290ecef752ffb900edc
charlie_bui <charlie@tigren.com>
2020-04-17 00:59:39 +0700

    facebook chat

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -43,7 +43,30 @@
 @include('layouts.block.footer')
 @yield('content-js')
 @include('home-page.partials.scripts')
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v6.0'
+        });
+    };
 
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+     attribution=setup_tool
+     page_id="110310680466394"
+     theme_color="#0c958f">
+</div>
 </body>
 
 </html>
