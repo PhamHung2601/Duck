3195c2272194e5deb1b9312c35170527c1f07f2f
Loky <doankhachung.bk@gmail.com>
2020-05-04 18:43:20 +0700

    fix share de thi

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -58,6 +58,27 @@
             xfbml            : true,
             version          : 'v6.0'
         });
+        window.facebookShare = function( callback ) {
+            var options = ({
+                    method : 'share',
+                    href   : 'https://dialithaytung.com'
+                }),
+                status = '';
+            FB.ui(options, function( response ){
+                if (response && !response.error_code) {
+                    status = 'success';
+                    $.event.trigger('fb-share.success');
+                } else {
+                    status = 'error';
+                    $.event.trigger('fb-share.error');
+                }
+                if(callback && typeof callback === "function") {
+                    callback.call(this, status);
+                } else {
+                    return response;
+                }
+            });
+        }
     };
 
     (function(d, s, id) {
