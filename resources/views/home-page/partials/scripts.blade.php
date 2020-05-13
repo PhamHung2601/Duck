160e0aaaccc9f28509ccd48baf0e7230799f71bc
ngan <ngan@azebiz.com>
2020-05-13 12:45:48 +0700

    tau commit

--- resources/views/home-page/partials/scripts.blade.php
+++ resources/views/home-page/partials/scripts.blade.php
@@ -1,4 +1,4 @@
 
 
 <script src="{{ asset('vendor/landing-page/js/bootstrap.bundle.min.js') }}"></script>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0&appId=872003539911688&autoLogAppEvents=1"></script>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0&appId=235076881084666&autoLogAppEvents=1"></script>
